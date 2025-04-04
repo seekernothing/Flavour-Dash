a3b7a5c282e2fa639fa3828c6d37c986267da181
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-04-04 18:12:19 +0530

    added classbased component...

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,34 @@
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>This is a about component</h2>
-    </div>
-  );
-};
+import UserClass from "./UserClass";
+import React from "react";
 
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <h2>This is a about component</h2>
+        <UserClass
+          name={"Abhishek Biradar"}
+          location={"Pune"}
+          number={"+918888888888"}
+        />
+      </div>
+    );
+  }
+}
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>About</h1>
+//       <h2>This is a about component</h2>
+// <UserClass name={"Abhishek Biradar"} location={"Pune"}  number={"+918888888888"}/>
+
+//     </div>
+//   );
+// };
 
 export default About;
---
+++ src/components/UserClass.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInfo: {
+        name: "Dummy",
+        location: "default",
+        avatar_url:"dummy url"
+      },
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const data = await fetch("https://api.github.com/users/seekernothing");
+      const json = await data.json(); // Add await here
+      console.log(json);
+
+      this.setState({
+        userInfo: json,
+      });
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  render() {
+    const { name, bio,avatar_url} = this.state.userInfo;
+
+    return (
+      <div className="user">
+      <img alt="avtar" src={avatar_url} />
+        <h2>Name:{name}</h2>
+
+        <h3>Bio:{bio}</h3>
+        
+      </div>
+    );
+  }
+}
+
+export default UserClass;
